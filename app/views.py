5e6596f110d9fdee223d6d265a5fb496d7ca7910
AndresMDV <andresqa@192.168.80.20>
2024-11-28 17:02:50 -0500

    completado

--- app/views.py
+++ app/views.py
@@ -1,38 +1,39 @@
 import json
-from django.http import Http404, JsonResponse
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.views import generic
 
 
 class ProductListView(generic.TemplateView):
-    template_name = 'list.html'
+    template_name = "list.html"
 
     def get(self, request, *args, **kwargs):
-        product_filter = request.GET.get('filter', None)
-        products = json.loads(open('products.json').read())['products']
+        product_filter = request.GET.get("filter", None)
+        products = json.loads(open("products.json").read())["products"]
 
         if product_filter:
-            if not request.headers.get('HX-Request'):
-                raise Http404('Should be an HTMX request')
-
             try:
-                products = [product for product in products if product['filterId'] == int(product_filter)]
-
-                return render(
-                    request,
-                    'list.html',
-                    {
-                        'products': products,
-                    },
-                )
+                products = [
+                    product
+                    for product in products
+                    if product["filterId"] == int(product_filter)
+                ]
 
             except json.JSONDecodeError:
-                return JsonResponse({'error': 'Invalid JSON payload'}, status=400)
+                return JsonResponse({"error": "Invalid JSON payload"}, status=400)
 
+        if request.headers.get("HX-Request"):
+            return render(
+                request,
+                "list.html",
+                {
+                    "products": products,
+                },
+            )
         return render(
             request,
-            'home.html',
+            "home.html",
             {
-                'products': products,
+                "products": products,
             },
         )
